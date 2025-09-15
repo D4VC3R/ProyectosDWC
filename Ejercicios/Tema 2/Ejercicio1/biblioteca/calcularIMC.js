61d186f8310e5fd3ab11dcbd5d5f15ec5418b8a4
D4VC3R <davcerval@alu.edu.gva.es>
2025-09-15 19:04:16 +0200

    Comienzo ejercicio1 tema 2

---
+++ Ejercicios/Tema 2/Ejercicio1/biblioteca/calcularIMC.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const Persona = {
+    masa,
+    altura
+};
+
+const Marcos = new Persona;
+Marcos.masa = 60;
+Marcos.altura = 180;
+
+const Juan = new Persona;
+Juan.masa = 70;
+Juan.altura = 177;
+
