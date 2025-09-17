abc7956f20ea6e1e12d37da5b8139e2fdcaae54d
D4VC3R <davcerval@alu.edu.gva.es>
2025-09-17 13:51:38 +0200

    Ejercicio1 completado

---
+++ Ejercicios/Tema 2/Entrega1/Ejercicio1/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import { getMes } from "./biblioteca/numToMes.js";
+
+// Comprobaciones
+console.log(getMes(1));
+console.log(getMes(12));
+console.log(getMes(5.4));
+console.log(getMes("F"));
