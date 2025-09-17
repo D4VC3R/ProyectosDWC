11e4c5f3646ba1e1f3e594d39b608347dd4daa18
D4VC3R <davcerval@alu.edu.gva.es>
2025-09-17 16:50:27 +0200

    Comprobación de si el numero es 0

--- Ejercicios/Tema 2/Entrega1/Ejercicio2/biblioteca/analisisNumerico.js
+++ Ejercicios/Tema 2/Entrega1/Ejercicio2/biblioteca/analisisNumerico.js
@@ -12,7 +12,7 @@ function isPrimo(num){
     // Comprobación de los dividores de cada número, si el módulo de algun posible divisor es 0, lanzamos el return.
     for (let i = 2; i < num; i++) {
         if (num % i == 0) {
-            return false;
+            return false; // Si entramos a este if, el numero no es primo.
         }
     }
     return true; // Si llegamos hasta aqui es que no hay divisores exactos y por lo tanto el número es primo.
@@ -22,7 +22,8 @@ function isPositivo(num){
     return num>0;
 };
 
-function isNumero(num){ // Un poco tontería esta función pero luego queda mas claro al leer la función analisisNumerico()
+// Un poco tontería esta función pero luego queda mas claro al leer la función analisisNumerico()
+function isNumero(num){ 
     return !isNaN(num);
 }
 
@@ -34,10 +35,14 @@ export function analisisNumerico(num){
     } else {
         return `${num} no es un número`; // Aqui no utilizo el operador ternario para poder utilizar el return en caso de que no se introduzca un numero.
     }
-    
-    isPar(num) ? resultado += `par, ` : resultado += `impar, `;
-    isPositivo(num) ? resultado += `positivo y ` : resultado += `negativo y `;
-    isPrimo(num) ? resultado += `primo.` : resultado += `no primo.`
+    // Ahora que estamos seguros de haber recibido un numero, lo analizamos.
+    if (num == 0) {
+        return resultado += `par, neutro y no primo.` // Nos quitamos el 0 ya de encima, que es muy molesto.
+    }else{
+        isPar(num) ? resultado += `par, ` : resultado += `impar, `;
+        isPositivo(num) ? resultado += `positivo y ` : resultado += `negativo y `;
+        isPrimo(num) ? resultado += `primo.` : resultado += `no primo.`;
+    }
 
     return resultado;
 }
