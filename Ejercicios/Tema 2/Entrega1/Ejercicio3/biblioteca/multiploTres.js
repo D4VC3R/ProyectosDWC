d177cc0de6d59f81728bfd06b1afb762d336d53d
D4VC3R <davcerval@alu.edu.gva.es>
2025-09-18 11:31:22 +0200

    Ejercicio 3

--- Ejercicios/Tema 2/Entrega1/Ejercicio3/biblioteca/multiploTres.js
+++ Ejercicios/Tema 2/Entrega1/Ejercicio3/biblioteca/multiploTres.js
@@ -2,17 +2,17 @@
 
 import { isPositivo, isNumero } from "../../Ejercicio2/biblioteca/analisisNumerico.js";
 
+// Como siempre, comprobamos que se introduce un entero positivo.
 export function getMultiplosTres(num){
     if(!isNumero(num) || !isPositivo(num))
         return "Introduce un número entero positivo.";
 
-    let resultado = "|";
+    let resultado = "|"; // Luego queda más bonito en consola.
 
+    // Vamos guardando en la variable resultado todos los multiplos de 3 hasta llegar al número introducido.
     for (let i = 1; i <= num; i++) {
         if (i%3===0) 
             resultado += ` ${i} |`;
-        
     }
-
     return resultado;
 }
--- Ejercicios/Tema 2/Entrega1/Ejercicio3/main.js
+++ Ejercicios/Tema 2/Entrega1/Ejercicio3/main.js
@@ -2,5 +2,9 @@
 
 import { getMultiplosTres } from "./biblioteca/multiploTres.js";
 
+// Comprobaciones
 console.log(getMultiplosTres(30));
 console.log(getMultiplosTres(4));
+console.log(getMultiplosTres("f"));
+console.log(getMultiplosTres(-2.5));
+console.log(getMultiplosTres(0));
