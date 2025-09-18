e02efb6f8880add400f3da2518ed4ca1a6fcc4d2
D4VC3R <davcerval@alu.edu.gva.es>
2025-09-18 11:31:35 +0200

    Ejercicio 4

--- Ejercicios/Tema 2/Entrega1/Ejercicio4/biblioteca/potencia.js
+++ Ejercicios/Tema 2/Entrega1/Ejercicio4/biblioteca/potencia.js
@@ -1,11 +1,13 @@
 "use strict";
 
+// Comprobamos que sean numeros enteros y además que el exponente sea 0 o positivo, que si no me rompes la función.
 export function getPotencia(base, exponente){
-    if (!Number.isInteger(base) || !Number.isInteger(exponente)) {
-        return "Solo te admito dos números enteros."
+    if (!Number.isInteger(base) || !Number.isInteger(exponente) || exponente < 0) {
+        return "Solo te admito dos números enteros. Además, el exponente lo quiero cero o positivo.";
     }
-    let resultado = 1;
+    let resultado = 1; // Inicializamos a 1 que es el elemento neutro de la multiplicación.
 
+    // Exponente actúa de contador de cuántas veces tenemos que multiplicar la base por si misma.
     while (exponente > 0) {
         resultado *= base;
         exponente--;
--- Ejercicios/Tema 2/Entrega1/Ejercicio4/main.js
+++ Ejercicios/Tema 2/Entrega1/Ejercicio4/main.js
@@ -2,6 +2,9 @@
 
 import { getPotencia } from "./biblioteca/potencia.js";
 
+// Comprobaciones
 console.log(getPotencia(2, 4));
 console.log(getPotencia(-3, 2));
 console.log(getPotencia(-3, 3));
+console.log(getPotencia(-3, -3));
+console.log(getPotencia("f", 3));
