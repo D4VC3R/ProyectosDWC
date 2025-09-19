4c77d31df2776a3843bf440691ed4ea8141447bd
D4VC3R <davcerval@alu.edu.gva.es>
2025-09-19 15:59:46 +0200

    Componente pelicula

---
+++ React/PeliculasApp/src/components/Ejercicio3/Pelicula.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Pelicula.css";
+
+const Pelicula = (props) => {
+
+  return (
+    <div className="Pelicula">
+
+
+        
+    </div>
+    );
+}
+
+export default Pelicula;
