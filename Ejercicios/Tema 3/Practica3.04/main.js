b7092aa4318556b04ede59358d91dba3572699ff
D4VC3R <davcerval@alu.edu.gva.es>
2025-09-30 14:18:11 +0200

    Arreglos en el main

--- Ejercicios/Tema 3/Practica3.04/main.js
+++ Ejercicios/Tema 3/Practica3.04/main.js
@@ -1,10 +1,10 @@
 "use strict";
 
+
 // Comprobaciones Ejercicio 1
+console.log("Ejercicio 1.");
 
 import { nombresPropios, pasarAMayus, ordenAlfabeticoInverso, addPropiedades} from "./biblioteca/ejercicios.js";
-
-console.log("Ejercicio 1.");
 
 const enMayuscula = pasarAMayus(nombresPropios);
 console.log(imprimirArray(enMayuscula));
@@ -15,15 +15,18 @@ console.log(imprimirArray(ordenInverso));
 const conPropiedades = addPropiedades(nombresPropios);
 console.log(conPropiedades);
 
+
 // Comprobaciones Ejercicio 2
+console.log("\nEjercicio 2.");
 
 import { getArrays, getArrayFinal, imprimirArray } from "./biblioteca/ejercicios.js";
-console.log("\nEjercicio 2.");
+
 const test = getArrays(3, 10, 1, 10);
 // console.log(test); 
 const testFiltrado = getArrayFinal(test, 5);
 // console.log(testFiltrado);
 console.log(imprimirArray(testFiltrado));
+
 
 // Comprobaciones Ejercicio 3
 console.log("\nEjercicio 3.");
@@ -32,11 +35,11 @@ import { usuarios } from "./biblioteca/Ejercicio3.js";
 import { nuevoUsuario, addUsuario, getMayoresDeEdad, getYahoo, getUsuariosFiltrados, getUsuariosIncompletos, addApellidos, addCodigoPostal } from "./biblioteca/ejercicios.js";
 
 
-console.log(usuarios);
-console.log(addUsuario(usuarios, nuevoUsuario));
-console.log(getMayoresDeEdad(usuarios));
-console.log(getYahoo(usuarios));
-console.log(getUsuariosFiltrados(usuarios));
-console.log(getUsuariosIncompletos(usuarios));
-console.log(addApellidos(usuarios));
-console.log(addCodigoPostal(usuarios));
+console.log("Usuarios originales:", usuarios);
+console.log("Usuario añadido:", addUsuario(usuarios, nuevoUsuario));
+console.log("Usuarios mayores de edad:", getMayoresDeEdad(usuarios));
+console.log("Usuarios con Yahoo:", getYahoo(usuarios));
+console.log("Usuarios españoles y mayores de edad con tema claro:", getUsuariosFiltrados(usuarios));
+console.log("Usuarios incompletos:", getUsuariosIncompletos(usuarios));
+console.log("Usuarios con el campo 'apellidos' añadido:", addApellidos(usuarios));
+console.log("Usuarios con el campo 'código' añadido:", addCodigoPostal(usuarios));
