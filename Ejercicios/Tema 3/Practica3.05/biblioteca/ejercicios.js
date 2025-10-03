6151360032c8864e1d60ccf57ea802f472fb7ed4
D4VC3R <davcerval@alu.edu.gva.es>
2025-10-03 16:15:16 +0200

    Ejercicio 2 empezado

--- Ejercicios/Tema 3/Practica3.05/biblioteca/ejercicios.js
+++ Ejercicios/Tema 3/Practica3.05/biblioteca/ejercicios.js
@@ -8,7 +8,7 @@ import { getNumeroAleatorio } from "../../Practica3.04/biblioteca/misFuncionesNu
 
 
 
-// Reutilizo la función que preparé para la práctica 3.04.
+// Parecida a la de la práctica 3.04 pero asegurando que no se repiten números.
 export const generarArrayAleatorioUnico = (min, max, elementosArray) => {
     const arrayAleatorio = [];
 		
@@ -25,4 +25,15 @@ export const generarArrayAleatorioUnico = (min, max, elementosArray) => {
 // Si el índice de un valor no es la última posición donde aparece, es que está repetido.
 export const comprobarArrayAleatorio = (arrayAleatorio = []) => {
     return arrayAleatorio.some((valor, indice, array) => array.indexOf(valor) !== array.lastIndexOf(valor));
+};
+
+                                                // -------------------------------
+                                                // Ejercicio 2 - El avance        |
+                                                // -------------------------------
+
+const generarArrayBidimensionalUnico = () => {
+	// Generamos un array con 9 números aleatorios no repetidos.
+	const numeros = generarArrayAleatorioUnico(1, 9, 9);
+	// A partir del array generado, generamos un array bidimensional
+	return [numeros.slice(0,3), numeros.slice(3,6), numeros.slice(6,9)];
 };
