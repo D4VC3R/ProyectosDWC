9ff7773ee1d3a99e93969872d54a43243f79f122
D4VC3R <davcerval@alu.edu.gva.es>
2025-10-16 10:41:53 +0200

    Practica4.02 completa

--- Ejercicios/Tema 4/Practica4.02/biblioteca/funcionesDOM.js
+++ Ejercicios/Tema 4/Practica4.02/biblioteca/funcionesDOM.js
@@ -1,49 +1,164 @@
 "use strict";
 
-                                                // -------------------------------
-                                                // Ejercicio 1 - El censor        |
-                                                // -------------------------------
+// -------------------------------
+// Ejercicio 1 - El censor        |
+// -------------------------------
 
-// Versión fácil  .                                              
+// Versión fácil  .
 export const censurarFacil = () => {
-    let cuerpo = document.body;
-    cuerpo.innerHTML = cuerpo.innerHTML.replaceAll("sexo", '<span class = "censura"> contenido bloqueado </span>');
+  const cuerpo = document.body;
+  cuerpo.innerHTML = cuerpo.innerHTML.replaceAll(
+    "sexo",
+    '<span class = "censura"> contenido bloqueado </span>'
+  );
 };
 
 // Versión "precisa".
 const censurar = (nodo) => {
-    nodo.innerHTML = nodo.innerHTML.replaceAll("sexo", '<span class = "censura"> contenido bloqueado </span>');
+  nodo.innerHTML = nodo.innerHTML.replaceAll(
+    "sexo",
+    '<span class = "censura"> contenido bloqueado </span>'
+  );
 };
 
-export const censurarEloyStyle = () => {
-    let cuerpo = document.body;
-    let nodos = cuerpo.children;
-
-    // Recorremos los nodos del body.
-    for (let i = 0; i < nodos.length; i++) {
-        const nodo = nodos[i];
-        //console.log(nodo.firstChild);
-        // Si el nodo tiene children y es de tipo texto, censuramos.
-        if (nodo.firstChild && typeof nodo.innerHTML === "string") {
-            censurar(nodo);
-        }
-    }
+const getNodos = (nodoPadre) => {
+  return nodoPadre.children;
 };
 
-                                                // --------------------------------
-                                                // Ejercicio 2 - Primos DOS        |
-                                                // --------------------------------
-                                                
+// Como necesito pasarle parámetro para hacerla recursiva, primero le paso el body para empezar a recorrer nodos.
+export const censurarNodos = (nodoPadre = document.body) => {
+  const nodos = getNodos(nodoPadre);
 
-export const crearTabla = () =>{
-    let cuerpo = document.body;
-    let tabla = document.createElement("table");
-    let fila = document.createElement("tr");
-    let celda = document.createElement("td");
+  // Recorremos los nodos del body. Si el nodo es de tipo texto, censuramos, y si además ese nodo tiene childrens, los comprobamos.
+  // He metido un <div> en el HTML para probarlo y parece que va bien.
+  for (let i = 0; i < nodos.length; i++) {
+    const nodo = nodos[i];
+    // console.log(nodo.firstChild);
+    nodo.TEXT_NODE && censurar(nodo);
+    nodo.hasChildNodes() && censurarNodos(nodo);
+  }
+};
 
-    cuerpo.innerHTML += tabla
-    for (let i = 1; i <= 10; i++) {
-        
-        
+// --------------------------------
+// Ejercicio 2 - Primos DOM        |
+// --------------------------------
+
+// Le paso parámetros para poder crear tablas del tamaño que quiera.
+export const crearTabla = (filas, columnas) => {
+  const cuerpo = document.body;
+  const tabla = document.createElement("table");
+
+  for (let i = 1; i <= filas; i++) {
+    const fila = document.createElement("tr");
+
+    for (let j = 1; j <= columnas; j++) {
+      const celda = document.createElement("td");
+      // {j} pinta las unidades e {i} pinta las decenas.
+      celda.textContent = (i - 1) * columnas + j;
+      fila.appendChild(celda);
     }
-}
+    tabla.appendChild(fila);
+  }
+  cuerpo.appendChild(tabla);
+};
+
+import { isPrimo } from "./misFuncionesNumericas.js";
+
+export const pintarPrimos = () => {
+  const tablas = document.getElementsByTagName("table");
+  // console.log(tablas)
+  // Hay que especificar que tabla vamos a manipular para poder tener acceso a un montón de métodos que facilitan la vida una barbaridad.
+  const tabla = tablas[0];
+  // console.log(tabla)
+  for (let i = 0; i < tabla.rows.length; i++) {
+    const fila = tabla.rows[i];
+
+    for (let j = 0; j < fila.cells.length; j++) {
+      const celda = fila.cells[j];
+      let numero = parseInt(celda.textContent);
+
+      if (isPrimo(numero)) {
+        celda.classList.add("isPrimo");
+      }
+    }
+  }
+};
+
+// ---------------------------------
+// Ejercicio 3 - Ocultar DOM        |
+// ---------------------------------
+
+import { getNumeroAleatorio } from "./misFuncionesNumericas.js";
+
+const colorAleatorio = () => {
+  return `rgb(${getNumeroAleatorio(0, 256)},${getNumeroAleatorio(0,256)},${getNumeroAleatorio(0, 256)})`;};
+
+// Crea un párrafo y lo devuelve, así puedo toquetearlo luego.
+const addParrafo = (id) => {
+  let p = document.createElement("p");
+  document.getElementById(id).appendChild(p);
+  return p;
+};
+
+export const addDiv = (id) => {
+  const div = document.createElement("div");
+  div.id = id;
+  document.body.appendChild(div);
+};
+
+// Selecciona un párrafo de los disponibles al azar y cambia sus atributos cada segundo.
+const cambiarColor = (id) => {
+  setInterval(() => {
+    const div = document.getElementById(id);
+    const parrafos = div.getElementsByTagName("p");
+
+    parrafos[getNumeroAleatorio(0, parrafos.length - 1)].setAttribute(
+      "style",
+      `background:${colorAleatorio()}; color:${colorAleatorio()};`
+    );
+  }, 1000);
+};
+
+export const parrafosLocos = (id) => {
+  for (let i = 0; i < 5; i++) {
+    const nuevoParrafo = addParrafo(id);
+    nuevoParrafo.innerHTML = `Párrafo ${i + 1}`;
+  }
+  cambiarColor(id);
+};
+
+// ----------------------------------
+// Ejercicio 4 - Carrusel DOM        |
+// ----------------------------------
+
+
+export const carrusel = (id) => {
+  let yamcha = ["./imgs/yamcha/0.png", "./imgs/yamcha/1.png", "./imgs/yamcha/2.png", "./imgs/yamcha/3.png", "./imgs/yamcha/4.png"];
+  let vegeta = ["./imgs/vegeta/0.png", "./imgs/vegeta/1.png","./imgs/vegeta/0 copy.png", "./imgs/vegeta/2.png", "./imgs/vegeta/3.png"];
+  const div = document.getElementById(id);
+  const imgYamcha = document.createElement("img");
+  const imgVegeta = document.createElement("img");
+  let indice = 0;
+  imgYamcha.classList.add("yamcha");
+  imgVegeta.classList.add("vegeta");
+
+  setInterval(() => {
+    div.appendChild(imgYamcha);
+    div.appendChild(imgVegeta);
+    imgVegeta.src = vegeta[indice];
+    imgYamcha.src = yamcha[indice];
+    // Que toggle acepte booleanos es una maraviila.
+    imgVegeta.classList.toggle("desplazar", indice === vegeta.length - 1);
+    // Cuándo índice valga lo mismo que la longitud del array, vuelve a 0.
+    indice = (indice + 1) % vegeta.length;
+  }, 1000);
+
+  /* Pensaba aplicar el intervalo solo para cambiar el atributo src, pero por algún motivo solo lo cambia si regenero el div por completo.
+  /* 
+  setInterval(() => {
+    indice = (indice + 1) % yamcha.length;  
+    img.src = yamcha[indice];
+    console.log("imagen:", indice); 
+  }, 1000);
+  */
+};
--- Ejercicios/Tema 4/Practica4.02/main.js
+++ Ejercicios/Tema 4/Practica4.02/main.js
@@ -2,12 +2,38 @@
 
 // Comprobaciones Ejercicio 1
 
-import { censurarFacil, censurarEloyStyle } from "./biblioteca/funcionesDOM.js";
+import { censurarFacil, censurarNodos } from "./biblioteca/funcionesDOM.js";
 
 /* setTimeout(() => {
     censurarFacil()
 }, 1000); */
 
 setTimeout(() =>{
-    censurarEloyStyle();
+    censurarNodos();
 }, 1000);
+
+// Comprobaciones Ejercicio 2
+
+import { crearTabla, pintarPrimos } from "./biblioteca/funcionesDOM.js";
+
+crearTabla(10, 10);
+setTimeout(() =>{
+    pintarPrimos();
+}, 1000);
+
+// Comprobaciones Ejercicio 3
+
+import { addDiv, parrafosLocos } from "./biblioteca/funcionesDOM.js";
+
+addDiv("ejercicio3");
+setTimeout(() => {
+    parrafosLocos("ejercicio3");
+}, 1000);
+
+// Comprobaciones Ejercicio 4
+
+import { carrusel } from "./biblioteca/funcionesDOM.js";
+
+addDiv("ejercicio4");
+carrusel("ejercicio4");
+
