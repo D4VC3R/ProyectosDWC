35cdfb84bbcae9dd46b98878865afe5f43bed709
D4VC3R <davcerval@alu.edu.gva.es>
2025-10-21 17:15:31 +0200

    Ajustes y modularización

--- React/PeliculasApp/src/App.jsx
+++ React/PeliculasApp/src/App.jsx
@@ -7,18 +7,12 @@ import Pelicula from "./components/Ejercicio3/Pelicula.jsx";
 
 
 function App() {
+	// Para la próxima práctica cambiaré la estructura de la carpeta components a una más acorde a un proyecto real en lugar de dividirla por prácticas.
 	return (
 		<>
 			<Contenedor>
 				<Peliculas />
 			</Contenedor>
-			<Contenedor>
-				<Pelicula nombre = "el sexto sentido" director = "Yo" 
-				cartelera = "https://www.ecartelera.com/carteles/5400/5409/001.jpg"
-				elenco = "Al Pacino">
-					</Pelicula>
-			</Contenedor>
-
 		</>
 	);
 }
--- React/PeliculasApp/src/components/Ejercicio3/Pelicula.jsx
+++ React/PeliculasApp/src/components/Ejercicio3/Pelicula.jsx
@@ -3,55 +3,57 @@ import "./Pelicula.css";
 import Interprete from "../Ejercicio2/Interprete";
 import { useRef } from "react";
 import { generarUuidAleatorio } from "../../libraries/misFunciones.js";
+import Elenco from "../Practica4.05/Elenco.jsx";
+import Taquilla from "../Practica4.05/Taquilla.jsx";
 
 
 const Pelicula = (props) => {
     let noInfo = "Sin información."
-    const taquillaRef = useRef(null);
-	const elencoRef = useRef(null);
 
+    // Creamos unas referencias para los contenedores de elenco y taquilla,
+    // de manera que le podamos quitar o poner la clase que los oculta.
+    const taquillaRef = useRef(null);
+    const elencoRef = useRef(null);
+
+    // Al quitarle la clase, se utilizan los estilos definidos en el css de cada componente.
     const mostrarElenco = () => {
-        elencoRef.current.classList.toggle("pelicula_elenco");
+        elencoRef.current.classList.toggle("pelicula_elenco_container");
     };
 
     const mostrarTaquilla = () => {
-        taquillaRef.current.classList.toggle("pelicula_taquilla");
+        taquillaRef.current.classList.toggle("pelicula_taquilla_container");
     };
 
 
     return (
-        <div className="pelicula_pelicula">
-            <div className="pelicula_imagen">
-                <img src={props.cartelera} className="pelicula_cartel" alt={props.titulo ? props.titulo : noInfo}></img>
-            </div>
-            <div className="pelicula_contenido">
-                <h1 className="pelicula_titulo">{props.titulo ? props.titulo : noInfo}</h1>
-                <h3 className="pelicula_director">Director: <em>{props.director ? props.director : noInfo}</em></h3>
-                <div className="pelicula_genero">Género: <em>{props.clasificacion ? props.clasificacion : noInfo}</em></div>
-                <div className="pelicula_resumen">{props.resumen ? props.resumen : noInfo}</div>
-            </div>
-            <div className="pelicula_botones">
-                <h2 className="pelicula_elenco_boton"> <button onClick={mostrarElenco}>Elenco</button></h2>
-                <h2 className="pelicula_taquilla_boton"> <button onClick={mostrarTaquilla}>Taquilla</button></h2>
-            </div>
-            <div ref = {taquillaRef} className="pelicula_taquilla_oculto">Recaudación: <em>{props.recaudacion ? props.recaudacion : noInfo}</em></div>
-            <div className="pelicula_elenco_container">
-                <div ref={elencoRef} className="pelicula_elenco_oculto">
-                    {props.actores ? props.actores.map((actor)=>{
-                        return (
-                            <Interprete
-                                key={generarUuidAleatorio()}
-                                nombre={actor.nombre}
-                                fechaNacimiento={actor.fechaNacimiento}
-                                imagen={actor.imagen}
-                                biografia={actor.biografia}
-                            />
-                        )
-                    }):noInfo}
+        <>
+            <div className="pelicula_pelicula">
+                <div className="pelicula_imagen">
+                    <img src={props.cartelera} className="pelicula_cartel" alt={props.titulo ? props.titulo : noInfo}></img>
+                </div>
+                <div className="pelicula_contenido">
+                    <h1 className="pelicula_titulo">{props.titulo ? props.titulo : noInfo}</h1>
+                    <h3 className="pelicula_director">Director: <em>{props.director ? props.director : noInfo}</em></h3>
+                    <div className="pelicula_genero">Género: <em>{props.clasificacion ? props.clasificacion : noInfo}</em></div>
+                    <div className="pelicula_resumen">{props.resumen ? props.resumen : noInfo}</div>
+                </div>
+
+                <div className="pelicula_botones">
+                    <h2 className="pelicula_elenco_boton"> <button onClick={mostrarElenco}>Elenco</button></h2>
+                    <h2 className="pelicula_taquilla_boton"> <button onClick={mostrarTaquilla}>Taquilla</button></h2>
                 </div>
             </div>
-        </div>
-);
+
+            <div ref={taquillaRef} className="pelicula_taquilla_container">
+                <Taquilla recaudacion={props.recaudacion} />
+            </div>
+
+            <div className="pelicula_elenco_container" ref={elencoRef}>
+                <Elenco actores={props.actores} />
+            </div>
+        </>
+
+    );
 
 };
 export default Pelicula;
