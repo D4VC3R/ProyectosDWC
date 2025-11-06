10e872519d522a64cd2afdb9462583262266674b
D4VC3R <davcerval@alu.edu.gva.es>
2025-11-06 20:34:52 +0100

    Comentarios añadidos

--- Ejercicios/Tema 5/Practica5.04/biblioteca/puzzle.js
+++ Ejercicios/Tema 5/Practica5.04/biblioteca/puzzle.js
@@ -6,6 +6,7 @@
 
 import { crearImg } from "./misFunciones.js";
 
+// Metemos la ruta a cada imagen en un array y las mezclamos.
 const getPiezas = () => {
   let piezas = ["./imgs/fila-1-columna-1.jpg",
     "./imgs/fila-1-columna-2.jpg",
@@ -29,6 +30,7 @@ const aleatorizarArray = (array) => {
   }
 }
 
+// Creamos y añadimos cada imagen del array que devuelve getPiezas() con id propio y el atributo 'draggable'.
 export const addPiezas = () => {
   const listadoPiezas = document.getElementById("piezas");
   let piezas = getPiezas();
@@ -41,10 +43,14 @@ export const addPiezas = () => {
   }
 }
 
+// Función para añadir una misma clase a todos los elementos de un array.
+// La utilizo para añadir clase a las casillas de la tabla.
 export const addClase = (clase, elementos = []) => {
   elementos.forEach(elemento => elemento.classList.add(clase));
 }
 
+// Identifico cada casilla con su posición, empezando en 1.
+// Así puedo aprovechar el src de cada imagen para comprobar si está en el sitio correcto.
 export const identificarCasillas = (casillas = []) => {
   let cont = 0;
   for (let i = 1; i <= 3; i++) {
@@ -54,18 +60,29 @@ export const identificarCasillas = (casillas = []) => {
     }
   }
 }
-
+// Primero nos quedamos con lo que hay después de la última '/'
+// y luego devolvemos el string antes del '.'.
 const limpiarSrc = (src = "") => {
   let posicion = src.split('/').pop();
   return posicion.split('.').shift();
 }
 
+// Si coincide el src de la imagen en la casilla, con el id de la propia casilla, está en la posición correcta.
+export const isCorrecta = (casilla) => {
+  return limpiarSrc(casilla.firstChild.src) === casilla.id;
+}
+
+// Si todas las casillas son correctas...
 export const isResuelto = (casillas = []) => {
   return casillas.every(casilla => isCorrecta(casilla));
 }
 
-export const isCorrecta = (casilla) => {
-  return limpiarSrc(casilla.firstChild.src) === casilla.id;
+// La utilizo cuando mueves una imagen de una casilla ya colocada, como tiene que tener si o si una de estas dos clases
+// con una ternaria nos aseguramos de que vamos a quitar la clase.
+export const limpiarClase = (casilla) => {
+  casilla.classList.contains("casilla-correcta")
+  ? casilla.classList.remove("casilla-correcta")
+  : casilla.classList.remove("casilla-incorrecta")
 }
 
 export const marcarCorrecta = (casilla) => {
@@ -78,12 +95,8 @@ export const marcarIncorrecta = (casilla) => {
   casilla.classList.add("casilla-incorrecta");
 }
 
-export const limpiarClase = (casilla) => {
-  casilla.classList.contains("casilla-correcta")
-  ? casilla.classList.remove("casilla-correcta")
-  : casilla.classList.remove("casilla-incorrecta")
-}
-
+// Devolvemos los elementos a su estado inicial, quitando las clases extra a las casillas
+// y devolviendo las imágenes a su contenedor original.
 export const reiniciar = (piezas = [], casillas = []) => {
   casillas.forEach(casilla => {
 		limpiarClase(casilla)
--- Ejercicios/Tema 5/Practica5.04/main.js
+++ Ejercicios/Tema 5/Practica5.04/main.js
@@ -15,11 +15,13 @@ window.onload = () => {
 	addPiezas();
 	insertarTabla(3, 3, "tablero");
 
+	// Pasamos las imágenes y las celdas de HTMLCollection a Array para jugar con los métodos.
 	const piezas = Array.from(document.getElementsByClassName("pieza"));
 	const casillas = Array.from(document.getElementsByTagName("TD"));
 	addClase("soltable", casillas);
 	identificarCasillas(casillas);
 
+	// Si el dragstart lo recibe una imagen, guardamos su id.
 	document.getElementById("contenedor-piezas").addEventListener("dragstart", (evento) => {
 		if (piezas.includes(evento.target)) {
 			evento.dataTransfer.setData("idImagen", evento.target.id);
@@ -30,13 +32,17 @@ window.onload = () => {
 		evento.preventDefault();
 	},false);
 
+	
 	document.getElementById("contenedor-piezas").addEventListener("drop", (evento) => {
 		evento.preventDefault();
+		// Nos aseguramos de que recibimos una imagen desde dataTransfer antes de añadir, 
+		// así evitamos errores feos en la consola si ALGUIEN intenta mover una casilla vacía al contenedor de imágenes.
 		const id = evento.dataTransfer.getData("idImagen");
 		if (!id) return;
 
 		document.getElementById("piezas").appendChild(document.getElementById(id));
 
+		// Si la imagen viene del contenedor-tablero, limpiamos la clase de la celda de la que viene.
 		const idCasillaAnterior = evento.dataTransfer.getData("idCasilla");
 		if (idCasillaAnterior) {
 			limpiarClase(document.getElementById(idCasillaAnterior));
@@ -49,19 +55,22 @@ window.onload = () => {
 
 	document.getElementById("contenedor-tablero").addEventListener("drop", (evento) => {
 		evento.preventDefault();
+		// De nuevo, comprobamos que no se esté intentanto arrastrar una casilla vacía a otra casilla.
 		const idImagen = evento.dataTransfer.getData("idImagen");
 		if (!idImagen) return;
 
-		const idCasillaAnterior = evento.dataTransfer.getData("idCasilla");
+		// Si se arrastra una imagen a una celda, la añadimos y comprobamos si está bien colocada.
 		if (evento.target.classList.contains("soltable")) {
 			evento.target.appendChild(document.getElementById(idImagen));
 			isCorrecta(evento.target) ? marcarCorrecta(evento.target) : marcarIncorrecta(evento.target);
-			
+
+			// Si la imagen no proviene de su contenedor original, limpiamos la clase de la casilla de donde proviene.
+			const idCasillaAnterior = evento.dataTransfer.getData("idCasilla");
 			if (idCasillaAnterior) {
 				limpiarClase(document.getElementById(idCasillaAnterior));
 			}
 		}
-
+		// Por último, comprobamos si era la última pieza y si están todas bien colocadas.
 		if (!document.getElementById("piezas").hasChildNodes() && isResuelto(casillas)) {
 			casillas.forEach(casilla => {
 				limpiarClase(casilla)
@@ -70,7 +79,7 @@ window.onload = () => {
 	},false);
 
 	document.getElementById("contenedor-tablero").addEventListener("dragstart", (evento) => {
-
+		// Si movemos una imagen ya colocada, guardamos el id de la imagen y de la casilla donde estaba.
 		if (piezas.includes(evento.target)) {
 			evento.dataTransfer.setData("idImagen", evento.target.id);
 			evento.dataTransfer.setData("idCasilla", evento.target.parentElement.id)
