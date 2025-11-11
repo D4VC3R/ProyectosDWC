3b31edf85d9ef678033eeaaefcf3574bbaa44bcb
D4VC3R <davcerval@alu.edu.gva.es>
2025-11-11 19:16:54 +0100

    caos

--- Ejercicios/Tema 5/Practica5.05/biblioteca/forms.js
+++ Ejercicios/Tema 5/Practica5.05/biblioteca/forms.js
@@ -7,36 +7,55 @@
 const validarTitulo = (nameForm, nameCampo) => {
 	const titulo = document.forms[nameForm][nameCampo].value;
 	const expTitulo = /^.{5,}$/;
-	return expTitulo.test(titulo);
+	let valido = expTitulo.test(titulo);
+	marcarCampo(nameCampo, valido);
+	return valido;
 }
 
 const validarInterprete = (nameForm, nameCampo) => {
 	const interprete = document.forms[nameForm][nameCampo].value;
 	const expInterprete = /^.{5,}$/;
-	return expInterprete.test(interprete);
+	let valido = expInterprete.test(interprete);
+	marcarCampo(nameCampo, valido);
+	return valido;
 }
 
 const validarAnyo = (nameForm, nameCampo) => {
 	const anyo = document.forms[nameForm][nameCampo].value;
 	const expAnyo = /[0-9]{4}/;
-	return expAnyo.test(anyo);
+	let valido = expAnyo.test(anyo);
+	marcarCampo(nameCampo, valido);
+	return valido;
 }
 
-const validarGenero = (idSelect) => {
-	return idSelect.selectedIndex > 0;
+const validarGenero = (nameForm, nameCampo) => {
+	const select = document.forms[nameForm][nameCampo];
+	let valido = select.value !== "";
+	marcarCampo(nameCampo, valido);
+	return valido;
 }
 
 const validarLocalizacion = (nameForm, nameCampo) => {
 	const localizacion = document.forms[nameForm][nameCampo].value;
 	const expLocalizacion = /^ES-\d{3}[A-Z]{2}$/;
-	return expLocalizacion.test(localizacion);
+	let valido = expLocalizacion.test(localizacion);
+	marcarCampo(nameCampo, valido);
+	return valido;
 }
 
-export const validarForm = (nameForm) => {
-	const formulario = document.forms[nameForm];
+export const comprobarForm = (nameForm) => {
 
-	return validarTitulo(formulario, formulario.titulo) &&
-	validarInterprete(formulario, formulario.interprete) &&
-	validarAnyo(formulario, formulario.anyo) &&
-	validarGenero(formulario, formulario.genero)
+	return validarTitulo(nameForm, "titulo") &&
+	validarInterprete(nameForm, "interprete") &&
+	validarAnyo(nameForm, "anyo") &&
+	validarGenero(nameForm, "genero") && 
+	validarLocalizacion(nameForm, "localizacion")
+}
+
+const marcarCampo = (valido, campo) => {
+	if (valido && campo.classList.contains("campo-invalido")) {
+		campo.classList.remove("campo-invalido");
+	}else if (!valido) {
+		campo.classList.add("campo-invalido");
+	}
 }
--- Ejercicios/Tema 5/Practica5.05/main.js
+++ Ejercicios/Tema 5/Practica5.05/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+import { comprobarForm } from "./biblioteca/forms.js";
+
+window.onload = () => {
+
+	document.getElementById("botonesForm").addEventListener("click", (evento) =>{
+		if (evento.target.id === "guardar") {
+			comprobarForm("formDiscos");
+			console.log(comprobarForm("formDiscos"));
+		}
+	}, false);
+}
