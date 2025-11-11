1207fe2b6a6dc7d7c018b5456255130599e69c78
D4VC3R <davcerval@alu.edu.gva.es>
2025-11-11 17:45:07 +0100

    Validaciones a medias

--- Ejercicios/Tema 5/Practica5.05/biblioteca/forms.js
+++ Ejercicios/Tema 5/Practica5.05/biblioteca/forms.js
@@ -4,24 +4,39 @@
 // Parte II                       |
 // -------------------------------
 
-const validarTitulo = (nameFormulario, nameTitulo) => {
-	const titulo = document.forms[nameFormulario][nameTitulo].value;
+const validarTitulo = (nameForm, nameCampo) => {
+	const titulo = document.forms[nameForm][nameCampo].value;
 	const expTitulo = /^.{5,}$/;
 	return expTitulo.test(titulo);
 }
 
-const validarInterprete = (nameFormulario, nameInterprete) => {
-	const interprete = document.forms[nameFormulario][nameInterprete].value;
+const validarInterprete = (nameForm, nameCampo) => {
+	const interprete = document.forms[nameForm][nameCampo].value;
 	const expInterprete = /^.{5,}$/;
 	return expInterprete.test(interprete);
 }
 
-const validarAnyo = (nameFormulario, nameAnyo) => {
-	const anyo = document.forms[nameFormulario][nameAnyo].value;
+const validarAnyo = (nameForm, nameCampo) => {
+	const anyo = document.forms[nameForm][nameCampo].value;
 	const expAnyo = /[0-9]{4}/;
 	return expAnyo.test(anyo);
 }
 
-const validarGenero = (nameFormulario, idSelect) => {
-	
+const validarGenero = (idSelect) => {
+	return idSelect.selectedIndex > 0;
+}
+
+const validarLocalizacion = (nameForm, nameCampo) => {
+	const localizacion = document.forms[nameForm][nameCampo].value;
+	const expLocalizacion = /^ES-\d{3}[A-Z]{2}$/;
+	return expLocalizacion.test(localizacion);
+}
+
+export const validarForm = (nameForm) => {
+	const formulario = document.forms[nameForm];
+
+	return validarTitulo(formulario, formulario.titulo) &&
+	validarInterprete(formulario, formulario.interprete) &&
+	validarAnyo(formulario, formulario.anyo) &&
+	validarGenero(formulario, formulario.genero)
 }
