d2bfad8b8f10bfd9db45900572d80f90640b0cdd
D4VC3R <davcerval@alu.edu.gva.es>
2025-11-15 21:00:21 +0100

    bastante encaminado

--- Ejercicios/Tema 5/Practica5.05/biblioteca/forms.js
+++ Ejercicios/Tema 5/Practica5.05/biblioteca/forms.js
@@ -8,18 +8,17 @@ export const getFormulario = (nameForm) => {
 	return document.forms[nameForm]
 }
 
-export const validarTitulo = (form, nameCampo) => {
-	const campo = form[nameCampo];
+// Como título e intérprete son obligatorios, valido si o si.
+export const validarTitulo = (campo) => {
 	const titulo = campo.value;
-	const expTitulo = /^.{5,}$/;
+	const expTitulo = /^.\w{4,}$/;
 
 	let valido = expTitulo.test(titulo);
 	marcarCampo(campo, valido);
 	return valido;
 }
 
-export const validarInterprete = (form, nameCampo) => {
-	const campo = form[nameCampo];
+export const validarInterprete = (campo) => {
 	const interprete = campo.value;
 	const expInterprete = /^.{5,}$/;
 	let valido = expInterprete.test(interprete);
@@ -27,69 +26,137 @@ export const validarInterprete = (form, nameCampo) => {
 	return valido;
 }
 
-export const validarAnyo = (form, nameCampo) => {
-	const campo = form[nameCampo];
+export const validarGenero = (campo) => {
+	let valido = campo.value !== "" && campo.value !== "reggaeton";
+	marcarCampo(campo, valido);
+	return valido;
+}
+
+// A partir de aquí, como son opcionales, solo valido si tienen contenido.
+export const validarAnyo = (campo) => {
 	const anyo = campo.value
-	const expAnyo = /[0-9]{4}/;
-	let valido = expAnyo.test(anyo);
-	marcarCampo(campo, valido);
+	const expAnyo = / ^(19|20)\d{2}/;
+	let valido = true;
+	
+	if (anyo.trim() !== "") {
+		valido = expAnyo.test(anyo);
+		marcarCampo(campo, valido);
+		return valido;
+	}
 	return valido;
 }
 
-export const validarGenero = (form, nameCampo) => {
-	const campo = form[nameCampo];
-	let valido = campo.value !== "";
-	marcarCampo(campo, valido);
-	return valido;
-}
-
-export const validarLocalizacion = (form, nameCampo) => {
-	const campo = form[nameCampo];
+export const validarLocalizacion = (campo) => {
 	const localizacion = campo.value
 	const expLocalizacion = /^ES-\d{3}[A-Z]{2}$/;
-	let valido = expLocalizacion.test(localizacion);
-	marcarCampo(campo, valido);
-	return valido;
-}
+	let valido = true;
 
-export const validarUrl = (form, nameCampo) => {
-	const campo = form[nameCampo];
-	const url = campo.value;
-	const expUrl = /^https?:\/\/.+\.(?:jpg|png)$/i;
-	let valido = expUrl.test(url);
-	marcarCampo(campo, valido);
+	if (localizacion.trim() !== ""){
+		valido = expLocalizacion.test(localizacion);
+		marcarCampo(campo, valido);
+		return valido;
+	}
 	return valido;
 }
 
 export const comprobarForm = (form) => {
 
-	return validarTitulo(form, "titulo") &&
-	validarInterprete(form, "interprete") &&
-	validarAnyo(form, "anyo") &&
-	validarGenero(form, "genero") && 
-	validarLocalizacion(form, "localizacion") &&
-	validarUrl(form, "caratula");
+	let arrayErrores = [];
+
+	if (!validarTitulo(form.titulo) ) {
+		arrayErrores = [...arrayErrores, "El título debe tener al menos 5 caracteres."];
+	}
+	if (!validarInterprete(form.interprete) ) {
+		arrayErrores = [...arrayErrores, "El intérprete debe tener al menos 5 caracteres."];
+	}
+	if (!validarAnyo(form.anyo)) {
+		arrayErrores = [...arrayErrores, "¿Seguro que tienes claro el año de publicación?"];
+	}
+	if (!validarGenero(form.genero)) {
+		arrayErrores = [...arrayErrores, "Lo siento, tienes que elegir un género musical."];
+	}
+	if (!validarLocalizacion(form.localizacion) ) {
+		arrayErrores = [...arrayErrores, "El formato, el formato, ¡el formato! Borra lo que hayas escrito y fíjate en el ejemplo."];
+	}
+	errores.length === 5 && (errores = [...errores, "Ya es difícil no rellenarme ni un solo campo bien..."]);
+
+	return arrayErrores;
+	
 }
 
-const moverError = (campo) => {
-	/**@type {HTMLSpanElement} */
-	const error = document.forms[0].getElementsByTagName("span")[0];
-	campo.parentNode.insertBefore(error, campo);
+export const mostrarError = (errores) => {
+
+	const listaErrores = document.getElementById("errores");
+	listaErrores.classList.toggle("oculto");
+	errores.map( error => {
+		const li = document.createElement("li");
+		li.textContent = error;
+		listaErrores.appendChild(li);
+	});
 }
 
-const mostrarError = () => {
-	const error = document.forms[0].getElementsByTagName("span")[0];
-	error.classList.toggle("oculto");
-	error.textContent = "Revisa este campo";
+export const limpiarErrores = () => {
+	const listaErrores = document.getElementById("errores");
+	listaErrores.innerHTML = "";
+	listaErrores.classList.add("oculto");
 }
 
 const marcarCampo = (campo, valido) => {
 	if (!valido){
 		campo.classList.add("campo-invalido");
-		moverError(campo);
-		mostrarError();
 	}
 	if (valido && campo.classList.contains("campo-invalido")) {
 		campo.classList.remove("campo-invalido");
 	}
+}
+
+const crearDisco = (form) => {
+
+	const disco = {
+		titulo: form.titulo.value,
+		interprete: form.interprete.value,
+		anyo: form.anyo?.value,
+		genero: form.genero?.value,
+		localizacion: form.localizacion?.value,
+		prestado: form.prestado?.value
+	};
+	return disco;
+}
+
+export const guardarDisco = (form, json) => {
+	const disco = crearDisco(form);
+	return [...json, disco];
+}
+
+export const mostrarDiscos = (json) => {
+	const tabla = document.getElementById("tablaDiscos");
+	document.getElementById("sinResultados").textContent = "";
+
+	json.forEach(disco => {
+		const fila = document.createElement("tr");
+		tabla.appendChild(fila);
+		for (const propiedad in disco) {
+			const celda = document.createElement("td");
+			celda.textContent = disco[propiedad] || "sin especificar";
+			fila.appendChild(celda);
+		}
+	});
+}
+
+export const limpiarDiscos = () => {
+  const tabla = document.getElementById("tablaDiscos");
+  tabla.innerHTML = "<tr><th>Título</th><th>Intérprete</th><th>Año</th><th>Género</th><th>Localización</th><th>Prestado</th></tr>";
+};
+
+// Si alguna propiedad del disco contiene el criterio de búsqueda, lo incluyo en los resultados.
+export const buscarDiscos = (json, busqueda) => {
+	const resultados = json.filter( disco => 
+		disco.titulo.toLowerCase().includes(busqueda.toLowerCase()) ||
+		disco.interprete.toLowerCase().includes(busqueda.toLowerCase()) ||
+		(disco.anyo && disco.anyo.includes(busqueda)) ||
+		(disco.genero && disco.genero.toLowerCase().includes(busqueda.toLowerCase())) ||
+		(disco.localizacion && disco.localizacion.toLowerCase().includes(busqueda.toLowerCase()))
+	);
+
+	return resultados;
 }
--- Ejercicios/Tema 5/Practica5.05/main.js
+++ Ejercicios/Tema 5/Practica5.05/main.js
@@ -1,28 +1,67 @@
 "use strict";
 
-import { comprobarForm, getFormulario, validarTitulo,validarGenero, validarAnyo, validarInterprete, validarLocalizacion, validarUrl } from "./biblioteca/forms.js";
+import { comprobarForm, getFormulario, validarTitulo,validarGenero, validarAnyo, validarInterprete, validarLocalizacion, mostrarError, limpiarErrores, guardarDisco, mostrarDiscos, limpiarDiscos, buscarDiscos } from "./biblioteca/forms.js";
 
 window.onload = () => {
 	const form = getFormulario("formDiscos");
+	let listaDiscos = [];
 
-	console.log(form.interprete)
 
 
-	document.getElementById("botonesForm").addEventListener("click", (evento) =>{
+	form.addEventListener("click", (evento) =>{
 		evento.preventDefault();
+		
 		if (evento.target.id === "guardar") {
-			console.log(comprobarForm(form));
+			limpiarErrores();
+			let errores = comprobarForm(form);
+			errores.length > 0
+				? mostrarError(errores)
+				: listaDiscos = guardarDisco(form, listaDiscos);
+		}
+
+		if (evento.target.id === "mostrar") {
+			limpiarDiscos();
+			mostrarDiscos(listaDiscos);
+		}
+
+		if (evento.target.id === "buscar") {
+			const criterio = form.busqueda.value;
+			limpiarDiscos();
+			let resultado = buscarDiscos(listaDiscos, criterio);
+			resultado.length !== 0
+			? mostrarDiscos(resultado)
+			: document.getElementById("sinResultados").textContent = "No se han encontrado discos que coincidan con la búsqueda.";
+		}
+		
+		if (evento.target.id === "limpiar") {
+			form.busqueda.value = "";
+			limpiarDiscos();
+			mostrarDiscos(listaDiscos);
 		}
 	}, false);
 
-	form.addEventListener("input", (evento)=> {
-		evento.target.id === "titulo" && validarTitulo(form, evento.target.id)
-		evento.target.id === "caratula" && validarUrl(form, evento.target.id)
-		evento.target.id === "interprete" && validarInterprete(form, evento.target.id)
-		evento.target.id === "anyo" && validarAnyo(form, evento.target.id)
-		evento.target.id === "genero" && validarGenero(form, evento.target.id)
-		evento.target.id === "localizacion" && validarLocalizacion(form, evento.target.id)
+	// He probado con input, change, focusin y blur, al final me quedo con input
+	form.addEventListener("change", (evento) => {
 		
+		switch (evento.target.id) {
+			case "titulo":
+				validarTitulo(evento.target);
+				break;
+			case "interprete":
+				validarInterprete(evento.target);
+				break;
+			case "anyo":
+				validarAnyo(evento.target);
+				break;
+			case "genero":
+				validarGenero(evento.target);
+				break;
+			case "localizacion":
+				validarLocalizacion(evento.target);
+				break;
+			default: 
+				break;
+		}
 	}, false)
 
 	
