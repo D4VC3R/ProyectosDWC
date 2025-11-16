cf37550efdefdb1e7fdd6b90aea22c26bc7926c6
D4VC3R <davcerval@alu.edu.gva.es>
2025-11-16 16:44:07 +0100

    ¿Terminado?

--- Ejercicios/Tema 5/Practica5.05/biblioteca/forms.js
+++ Ejercicios/Tema 5/Practica5.05/biblioteca/forms.js
@@ -5,174 +5,268 @@
 // -------------------------------
 
 export const getFormulario = (nameForm) => {
-	return document.forms[nameForm]
-}
+  return document.forms[nameForm];
+};
 
-// Como título e intérprete son obligatorios, valido si o si.
+// Como título e intérprete son obligatorios, valido si o si. Género al tener que elegir uno si o si, lo he considerado obligatorio también.
 export const validarTitulo = (campo) => {
-	const titulo = campo.value;
-	const expTitulo = /^.\w{4,}$/;
+  const titulo = campo.value;
+  const expTitulo = /^.{5,}$/;
 
-	let valido = expTitulo.test(titulo);
-	marcarCampo(campo, valido);
-	return valido;
-}
+  let valido = expTitulo.test(titulo);
+  marcarCampo(campo, valido);
+  return valido;
+};
 
 export const validarInterprete = (campo) => {
-	const interprete = campo.value;
-	const expInterprete = /^.{5,}$/;
-	let valido = expInterprete.test(interprete);
-	marcarCampo(campo, valido);
-	return valido;
-}
+  const interprete = campo.value;
+  const expInterprete = /^.{5,}$/;
+  let valido = expInterprete.test(interprete);
+  marcarCampo(campo, valido);
+  return valido;
+};
 
 export const validarGenero = (campo) => {
-	let valido = campo.value !== "" && campo.value !== "reggaeton";
-	marcarCampo(campo, valido);
-	return valido;
-}
+  let valido = campo.value !== "" && campo.value !== "reggaeton";
+  marcarCampo(campo, valido);
+  return valido;
+};
 
 // A partir de aquí, como son opcionales, solo valido si tienen contenido.
 export const validarAnyo = (campo) => {
-	const anyo = campo.value
-	const expAnyo = /^(19|20)\d{2}$/;
-	let valido = true;
-	
-	if (anyo.trim() !== "") {
-		valido = expAnyo.test(anyo);
-		marcarCampo(campo, valido);
-		return valido;
-	}
-	return valido;
-}
+  const anyo = campo.value;
+  const expAnyo = /^(19|20)\d{2}$/;
+  let valido = true;
+
+  if (anyo.trim() !== "") {
+    valido = expAnyo.test(anyo);
+    marcarCampo(campo, valido);
+    return valido;
+  }
+  return valido;
+};
 
 export const validarLocalizacion = (campo) => {
-	const localizacion = campo.value
-	const expLocalizacion = /^ES-\d{3}[A-Z]{2}$/;
-	let valido = true;
+  const localizacion = campo.value;
+  const expLocalizacion = /^ES-\d{3}[A-Z]{2}$/;
+  let valido = true;
 
-	if (localizacion.trim() !== ""){
-		valido = expLocalizacion.test(localizacion);
-		marcarCampo(campo, valido);
-		return valido;
-	}
-	return valido;
-}
+  if (localizacion.trim() !== "") {
+    valido = expLocalizacion.test(localizacion);
+    marcarCampo(campo, valido);
+    return valido;
+  }
+  return valido;
+};
 
+// Al principio pensé en devolver un booleano, si todo valida devuelvo true, pero eso me complicaba luego mostrar los mensajes de error específicos.
+// Devolviendo un array, si está vacío sé que todo ha ido bien y si no, ya tengo los mensajes de error según lo que haya fallado.
 export const comprobarForm = (form) => {
+  let arrayErrores = [];
 
-	let arrayErrores = [];
+  if (!validarTitulo(form.titulo)) {
+    arrayErrores = [...arrayErrores, "El título debe tener al menos 5 caracteres."];
+  }
+  if (!validarInterprete(form.interprete)) {
+    arrayErrores = [...arrayErrores, "El intérprete debe tener al menos 5 caracteres."];
+  }
+  if (!validarAnyo(form.anyo)) {
+    arrayErrores = [...arrayErrores, "¿Seguro que tienes claro el año de publicación?"];
+  }
+  if (!validarGenero(form.genero)) {
+    arrayErrores = [...arrayErrores, "Lo siento, tienes que elegir un género musical."];
+  }
+  if (!validarLocalizacion(form.localizacion)) {
+    arrayErrores = [...arrayErrores, "El formato, el formato, ¡el formato! Borra lo que hayas escrito y fíjate en el ejemplo."];
+  }
+  arrayErrores.length === 5 && (arrayErrores = [...arrayErrores, "Ya es difícil no rellenarme ni un solo campo bien..."]);
 
-	if (!validarTitulo(form.titulo) ) {
-		arrayErrores = [...arrayErrores, "El título debe tener al menos 5 caracteres."];
-	}
-	if (!validarInterprete(form.interprete) ) {
-		arrayErrores = [...arrayErrores, "El intérprete debe tener al menos 5 caracteres."];
-	}
-	if (!validarAnyo(form.anyo)) {
-		arrayErrores = [...arrayErrores, "¿Seguro que tienes claro el año de publicación?"];
-	}
-	if (!validarGenero(form.genero)) {
-		arrayErrores = [...arrayErrores, "Lo siento, tienes que elegir un género musical."];
-	}
-	if (!validarLocalizacion(form.localizacion) ) {
-		arrayErrores = [...arrayErrores, "El formato, el formato, ¡el formato! Borra lo que hayas escrito y fíjate en el ejemplo."];
-	}
-	arrayErrores.length === 5 && (arrayErrores = [...arrayErrores, "Ya es difícil no rellenarme ni un solo campo bien..."]);
+  return arrayErrores;
+};
 
-	return arrayErrores;
-	
+export const mostrarExito = () => {
+	const mensajeExito = document.getElementById("exito");
+	mensajeExito.textContent = "Disco guardado correctamente.";
+	mensajeExito.classList.toggle("oculto");
 }
-
+// Rellenamos la lista con tantos errores como haya devuelto comprobarForm y los hacemos visibles al usuario.
 export const mostrarError = (errores) => {
+  const listaErrores = document.getElementById("errores");
+  listaErrores.classList.toggle("oculto");
 
-	const listaErrores = document.getElementById("errores");
-	listaErrores.classList.toggle("oculto");
-	errores.map( error => {
-		const li = document.createElement("li");
-		li.textContent = error;
-		listaErrores.appendChild(li);
-	});
+  errores.map((error) => {
+    const li = document.createElement("li");
+    li.textContent = error;
+    listaErrores.appendChild(li);
+  });
+};
+
+export const ocultarExito = () => {
+	const mensajeExito = document.getElementById("exito");
+	mensajeExito.classList.add("oculto");
 }
 
+// Para que no se me acumulen los errores en cada intento de envío, vacío la lista con innerHTML y la oculto otra vez.
 export const limpiarErrores = () => {
-	const listaErrores = document.getElementById("errores");
-	listaErrores.innerHTML = "";
-	listaErrores.classList.add("oculto");
-}
+  const listaErrores = document.getElementById("errores");
+  listaErrores.innerHTML = "";
+  listaErrores.classList.add("oculto");
+};
 
 const marcarCampo = (campo, valido) => {
-	if (!valido){
-		campo.classList.add("campo-invalido");
-	}
-	if (valido && campo.classList.contains("campo-invalido")) {
-		campo.classList.remove("campo-invalido");
-	}
-}
-// Está muy feo pero es para que cada disco tenga un id y poder borrarlos facilmente.
-let contId = 1;
+  if (!valido) {
+    campo.classList.add("campo-invalido");
+  }
+  if (valido && campo.classList.contains("campo-invalido")) {
+    campo.classList.remove("campo-invalido");
+  }
+};
+
+// A tope con la modularidad.
 const crearDisco = (form) => {
+  const disco = {
+    id: crypto.randomUUID(),
+    titulo: form.titulo.value,
+    interprete: form.interprete.value,
+    anyo: form.anyo?.value,
+    caratula: form.caratula?.value,
+    genero: form.genero?.value,
+    localizacion: form.localizacion?.value,
+    prestado: form.prestado?.value,
+  };
+  return disco;
+};
+// Nos creamos un disco a partir del formulario y con el desparrame, lo añadimos a la colección.
+export const guardarDisco = (form, coleccion) => {
+  const disco = crearDisco(form);
+  return [...coleccion, disco];
+};
 
-	const disco = {
-		id: contId++,
-		titulo: form.titulo.value,
-		interprete: form.interprete.value,
-		anyo: form.anyo?.value,
-		genero: form.genero?.value,
-		localizacion: form.localizacion?.value,
-		prestado: form.prestado?.value
-	};
-	return disco;
-}
+// Aquí me he complicado un poco de más la vida, si en lugar de una tabla hubiese hecho como en React con las películas, creando un div por cada disco, habría sido mejor.
+// Pero en principio lo planteé así, asi que he tirado para adelante.
+const mostrarDisco = (disco) => {
+    const fila = document.createElement("tr");
+		// Para asegurarme de que el atributo del disco se dibuje en la columna correcta.
+    const columnas = [
+        "caratula",
+        "titulo",
+        "interprete",
+        "anyo",
+        "genero",
+        "localizacion",
+        "prestado",
+    ];
 
-export const guardarDisco = (form, json) => {
-	const disco = crearDisco(form);
-	return [...json, disco];
-}
+    columnas.forEach((columna) => {
+        const celda = document.createElement("td");
+
+        if (columna === "caratula") {
+            const img = document.createElement("img");
+            img.src = disco.caratula || "";
+            img.alt = `Carátula de ${disco.titulo}`;
+            img.className = "caratula";
+            celda.appendChild(img);
+        } else if (columna === "genero") {
+            celda.textContent = formatearGenero(disco[columna]);
+        } else if (columna === "prestado") {
+            celda.textContent = formatearEstado(disco[columna]);
+        } else {
+            celda.textContent = disco[columna] || "Sin especificar";
+        }
+        fila.appendChild(celda);
+    });
+    const celdaBoton = document.createElement("td");
+    celdaBoton.appendChild(botonEliminar(disco.id));
+    fila.appendChild(celdaBoton);
+		// Necesito la fila completa para añadirla a la tabla.
+    return fila;
+};
 
 export const mostrarDiscos = (json) => {
-	const tabla = document.getElementById("tablaDiscos");
-	document.getElementById("sinResultados").textContent = "";
+    const tabla = document.getElementById("tablaDiscos");
+    document.getElementById("sinResultados").textContent = "";
 
-	json.forEach(disco => {
-		const fila = document.createElement("tr");
-		tabla.appendChild(fila);
+    json.forEach((disco) => {
+        tabla.appendChild(mostrarDisco(disco));
+    });
+};
 
-		for (const propiedad in disco) {
-			const celda = document.createElement("td");
-			celda.textContent = disco[propiedad] || "sin especificar";
-			fila.appendChild(celda);
-		}
-		fila.appendChild(document.createElement("td").appendChild(botonEliminar(disco.id)));
-	});
-}
+const formatearGenero = (genero) => {
+  switch (genero) {
+    case "rock":
+      return "Rock";
+    case "popEs":
+      return "Pop Español";
+    case "popInt":
+      return "Pop Internacional";
+    case "rap":
+      return "Rap / Hip-Hop";
+    case "clasica":
+      return "Música Clásica";
+    case "techno":
+      return "Techno";
+    case "reggae":
+      return "Reggae";
+    default:
+      return "Sin especificar";
+  }
+};
 
+const formatearEstado = (prestado) => {
+  return prestado === "false" ? "No" : "Sí";
+};
+
+// Dejo solo la cabecera de la tabla y me cargo lo que haya dentro de ella.
 export const limpiarListado = () => {
   const tabla = document.getElementById("tablaDiscos");
-  tabla.innerHTML = "<tr><th>ID</th><th>Título</th><th>Intérprete</th><th>Año</th><th>Género</th><th>Localización</th><th>Prestado</th><th>Eliminar</th></tr>";
+  tabla.innerHTML =
+    "<tr><th>Carátula</th><th>Título</th><th>Intérprete</th><th>Año</th><th>Género</th><th>Localización</th><th>Prestado</th><th>Eliminar</th></tr>";
 };
+
+// -------------------------------
+// Parte III                      |
+// -------------------------------
 
 // Si alguna propiedad del disco contiene el criterio de búsqueda, lo incluyo en los resultados.
 export const buscarDiscos = (json, busqueda) => {
-	const resultados = json.filter( disco => 
-		disco.titulo.toLowerCase().includes(busqueda.toLowerCase()) ||
-		disco.interprete.toLowerCase().includes(busqueda.toLowerCase()) ||
-		(disco.anyo && disco.anyo.includes(busqueda)) ||
-		(disco.genero && disco.genero.toLowerCase().includes(busqueda.toLowerCase())) ||
-		(disco.localizacion && disco.localizacion.toLowerCase().includes(busqueda.toLowerCase()))
-	);
+  const resultados = json.filter((disco) =>
+      disco.titulo.toLowerCase().includes(busqueda.toLowerCase()) ||
+      disco.interprete.toLowerCase().includes(busqueda.toLowerCase()) ||
+      (disco.anyo && disco.anyo.includes(busqueda)) ||
+      (disco.genero && disco.genero.toLowerCase().includes(busqueda.toLowerCase())) ||
+      (disco.localizacion && disco.localizacion.toLowerCase().includes(busqueda.toLowerCase()))
+  );
+  return resultados;
+};
 
-	return resultados;
-}
+// -------------------------------
+// Parte IV                       |
+// -------------------------------
 
+// Como tiene que aparecer un botón por cada disco, lo tengo que crear dinámicamente.
 const botonEliminar = (id) => {
-	const boton = document.createElement("button");
-	boton.className = "borrar";
-	boton.id = id;
-	boton.textContent = "X";
-	return boton;
-}
+  const boton = document.createElement("button");
+  boton.className = "borrar";
+  boton.id = id;
+  boton.textContent = "X";
+  return boton;
+};
 
+// Clásico filter para devolver todo el array menos el disco elegido.
 export const eliminarDisco = (json, id) => {
-	return json.filter( disco => disco.id !== id);
-}
+  return json.filter((disco) => disco.id !== id);
+};
+
+// -------------------------------
+// Parte V                        |
+// -------------------------------
+
+// Devuelve el listado para almacenarlo en una variable cuando carguemos la página.
+export const getListadoDiscos = () => {
+  return JSON.parse(localStorage.getItem("listadoDiscos"));
+};
+
+// Le pasamos la colección actualizada para guardarla en el localStorage.
+export const guardarListadoDiscos = (coleccion) => {
+  localStorage.setItem("listadoDiscos", JSON.stringify(coleccion));
+};
--- Ejercicios/Tema 5/Practica5.05/main.js
+++ Ejercicios/Tema 5/Practica5.05/main.js
@@ -1,47 +1,60 @@
 "use strict";
 
-import { comprobarForm, getFormulario, validarTitulo,validarGenero, validarAnyo, validarInterprete, validarLocalizacion, mostrarError, limpiarErrores, guardarDisco, mostrarDiscos, limpiarListado, buscarDiscos, eliminarDisco } from "./biblioteca/forms.js";
+import { comprobarForm, getFormulario, validarTitulo,validarGenero, validarAnyo, validarInterprete, validarLocalizacion, mostrarError, 
+	limpiarErrores, guardarDisco, mostrarDiscos, limpiarListado, buscarDiscos, eliminarDisco, guardarListadoDiscos, getListadoDiscos, mostrarExito, ocultarExito } from "./biblioteca/forms.js";
 
 window.onload = () => {
 	const form = getFormulario("formDiscos");
-	let listaDiscos = [];
-
+	// Cargamos el listado de discos, si no hay nada, será un array vacío.
+	let listaDiscos = getListadoDiscos() || [];
 
 
 	form.addEventListener("click", (evento) =>{
 		evento.preventDefault();
-		
+		console.log(evento.target)
+		// Cada vez que se intente guardar, hay que limpiar los mensajes previos para no acabar con una lista infinita de errores.
 		if (evento.target.id === "guardar") {
 			limpiarErrores();
+			ocultarExito();
 			let errores = comprobarForm(form);
-			errores.length > 0
-				? mostrarError(errores)
-				: listaDiscos = guardarDisco(form, listaDiscos);
+			// Si comprobarForm devuelve un array vacío, hemos triunfado. Guardamos el disco en la lista, la lista en el localStorage, limpiamos el formulario y chuche para el usuario.
+			if (errores.length === 0){
+				listaDiscos = guardarDisco(form, listaDiscos);
+				guardarListadoDiscos(listaDiscos);
+				form.reset();
+				mostrarExito();
+			} else {
+				mostrarError(errores);
+			}
 		}
-
+		// Para no duplicar contenido, limpio el listado antes de mostrarlo de nuevo. Si hay algún mensaje de éxito previo, ya habrá tenido tiempo de leerlo así que me lo cargo.
 		if (evento.target.id === "mostrar") {
 			limpiarListado();
+			ocultarExito();
 			mostrarDiscos(listaDiscos);
 		}
-
+		// Lo mismo, limpiamos lo que haya y mostramos lo que haya encontrado (o no) la búsqueda.
 		if (evento.target.id === "buscar") {
 			const criterio = form.busqueda.value;
+			ocultarExito();
 			limpiarListado();
 			let resultado = buscarDiscos(listaDiscos, criterio);
 			resultado.length !== 0
 			? mostrarDiscos(resultado)
 			: document.getElementById("sinResultados").textContent = "No se han encontrado discos que coincidan con la búsqueda.";
 		}
-
+		// Limpiamos el campo de búsqueda, el listado y cualquier mensaje de éxito que pudiera haber.
 		if (evento.target.id === "limpiar") {
 			form.busqueda.value = "";
 			limpiarListado();
-			mostrarDiscos(listaDiscos);
+			ocultarExito();
 		}
 	}, false);
 
-	// He probado con input, change, focusin y blur, al final me quedo con input
+	// He probado con input, change, focusin y blur, al final me quedo con input, blur prometía bastante pero no he sabido hacerlo funcionar.
 	form.addEventListener("change", (evento) => {
+		// Otra vez esta función, si el usuario es un máquina y después de añadir bien un disco quiere añadir otro, no tiene sentido mantener el mensaje de éxito en el momento se pone a rellenar campos de nuevo.
+		ocultarExito();
 		
 		switch (evento.target.id) {
 			case "titulo":
@@ -66,11 +79,26 @@ window.onload = () => {
 
 	document.getElementById("contenedorDiscos").addEventListener("click", (evento) => {
 		if (evento.target.classList.contains("borrar")){
-			listaDiscos = eliminarDisco(listaDiscos, parseInt(evento.target.id));
-			limpiarListado();
-			mostrarDiscos(listaDiscos);
+			// Si, otra vez.
+			ocultarExito();
+			if (confirm("¿Seguro que quieres borrar este disco?")) {
+				// Si confirma, quitamos el disco de la lista, guardamos la lista actualizada, limpiamos y mostramos la nueva lista.
+				listaDiscos = eliminarDisco(listaDiscos, evento.target.id);
+				guardarListadoDiscos(listaDiscos);
+				limpiarListado();
+				mostrarDiscos(listaDiscos);
+			}
 		}
 	}, false);
 
+	/** Divagaciones:
+	 * Lo que menos me ha gustado es tener que usar ocularExito() en tantos sitios, pero no se me ha ocurrido otra forma de hacerlo.
+	 * Según creo, en el momento el usuario se pone a toquetear cualquier cosa del formulario, el mensaje de éxito ya no le interesa y no tiene sentido mantenerlo.
+	 * 
+	 * Y me hubiera gustado hacer el popup de error que saliera al lado del campo que falla, con un appendChild al campo no válido de un <span> con el mensaje de error, 
+	 * pero no he sabido hacerlo funcionar bien y quedaba fatal, me descuadraba el formulario, me complicaba el código... al final lo he dejado. Cuando mi CSS no de pena igual me animo a hacerlo.
+	 * 
+	 * Es la práctica que más me ha costado sacar hasta ahora, con diferencia.
+	 */
 	
-}
+}// Fin del window.onload
