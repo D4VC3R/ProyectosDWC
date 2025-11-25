d32d32d6b4a74ec5309db5f2ee2aaba8de85f5a4
D4VC3R <davcerval@alu.edu.gva.es>
2025-11-25 12:31:27 +0100

    Probando el asicronismoooooorl

--- Ejercicios/Tema 6/Practica6.01/main.js
+++ Ejercicios/Tema 6/Practica6.01/main.js
@@ -1,13 +1,13 @@
 "use strict";
 
-import { plantilla } from "./biblioteca/promesas.js";
+import { ordenar, plantilla, plantillaSW } from "./biblioteca/promesas.js";
 
 
 window.onload = () => {
 
 	// Ejercicio 1
 
-	const promesa = new Promise((resolver, rechazar) =>{
+/* 	const promesa = new Promise((resolver, rechazar) =>{
 		setTimeout(() => {
 			const num = Math.floor(Math.random()*101);
 			num % 2 === 0
@@ -24,7 +24,49 @@ window.onload = () => {
 	})
 	.finally(()=>{
 		console.log("Proceso finalizado.")
-	})
+	}) */
 
 	// Ejercicio 2
+
+/* 	const fichero = './feos.json';
+	const fichro = "";
+	const div = document.getElementById("feos");
+	let haFallao;
+
+	fetch(fichero)
+	.then((respuesta)=>{
+		return respuesta.json();
+	})
+	.then((datos)=>{
+		let ordenados = ordenar(datos);
+		ordenados.map((feo)=>{
+			return div.innerHTML += plantilla(feo);
+		});
+	}).catch((error)=>{
+		haFallao = error.message;
+	}).finally(()=>{
+		haFallao ? console.log(haFallao) : console.log("Datos cargados.");
+	}) */
+
+	// Ejercicio 3
+
+	const swapi = "https://swapi.info/api/people";
+	const gentuza = document.getElementById("gentuza");
+	fetch(swapi)
+	.then((respuesta)=>{
+		return respuesta.json();
+	})
+	.then((datos)=>{
+		datos.map((personaje)=>{
+			return gentuza.innerHTML += plantillaSW(personaje);
+		});
+	})
+	.catch((error)=>{
+		console.log(error)
+	})
+	.finally(()=>{
+		console.log("Datos cargados.")
+	})
+
+
 }
