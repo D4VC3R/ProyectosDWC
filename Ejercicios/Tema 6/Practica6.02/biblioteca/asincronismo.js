b86134dd1960dfa1f9df8c8520407a3003cd8762
D4VC3R <davcerval@alu.edu.gva.es>
2025-11-28 20:19:31 +0100

    minifixes

--- Ejercicios/Tema 6/Practica6.02/biblioteca/asincronismo.js
+++ Ejercicios/Tema 6/Practica6.02/biblioteca/asincronismo.js
@@ -9,15 +9,14 @@ export const traerDatos = (url) => {
 	return (
 		fetch(url)
 		.then((respuesta) =>{
-			if (!respuesta.ok) {
-				throw new Error("Se cayó la API... otra vez.");
-			}
 			return respuesta.json();
 		})
 		.then((datos)=>{
 			return datos;
+		}).catch((error) =>{
+			throw new Error("Se cayó la API... otra vez.");
 		})
-		// No devuelvo el error porque en caso de haberlo, lo guardaría en la promesa (peliculasSW en el main).
-		// Si se produce un error, lo capturo en el try catch del main.
+		// No devuelvo el error porque en caso de hacerlo, lo guardaría en la promesa (peliculasSW en el main).
+		// Si se produce un error, lo capturo aquí y lanzo otro con un mensaje personalizado para capturarlo en el main.
 	);
 };
